90c47a550202774e61a69e4f1d4e5d0c236d3f8c
Sayan <sayandg41@gmail.com>
2024-01-31 11:57:45 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css"
+function App() {
+  return (<div className="card">
+    <Avatar />
+    <div className="data">
+      <Intro />
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+      <SkillList />
+    </div>
+  </div>
+  )
+}
+
+function Avatar() {
+  return <img className="avatar" src="sherlock.jpeg" alt="Error" />;
+}
+function Intro() {
+    return (<div>
+        <h1>Sherlock Holmes</h1>
+        <p>Sherlock Holmes is a fictional detective created by British author Arthur Conan Doyle. Referring to himself as a "consulting detective" in the stories,
+             Holmes is known for his proficiency with observation.</p>
+    </div>)
+}
+function SkillList() {
+ return(
+  <div className="skill-list">
+    <Skill skill="React" emoji="💻" color='red' />
+    <Skill skill="Javascript" emoji="🤞" color='yellow' />
+    <Skill skill="HTML" emoji="🫡" color='orange' />
+    <Skill skill="CSS" emoji="👌" color='blue' />
+    <Skill skill="TypeScript" emoji="😒" color='grey' />
+    <Skill skill="NextJS" emoji="✌️" color='green' />
+    <Skill skill="NodeJs" emoji="😎" color='#157894' />
+  </div>
+ ) 
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{backgroundColor: props.color}}>
+      {props.skill}
+      {props.emoji}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+ <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
